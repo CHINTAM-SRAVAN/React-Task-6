f7f0d144e792dd04fe735e85c213d31f0092c726
CHINTAM-SRAVAN-KUMAR <afsravan@gmail.com>
2023-10-11 11:52:11 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Chintam Sravan Kumar",
+    "role": "BTech Student",
+    "linkedinId":"chintam-sravan-kumar",
+    "roleDescription": "I am interested in Data Analytics, which involves examining and making sense of data to help with decision-making. I'm also keen on Artificial Intelligence, which focuses on creating smart computer systems that can do things like humans do, opening up exciting possibilities in technology.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/chintam-sravan-kumar-ab81a021b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/CHINTAM-SRAVAN",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"I am currently a pre-final year student at Vellore Institute of Technology and pursuing my B.Tech from here. I took specialization in Data Analytics, currently diving into Artificial Intelligence and Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://www.linkedin.com/in/chintam-sravan-kumar-ab81a021b/",
+    "education":[
+      {
+        "UniversityName":"Vellore Institute of Technology",
+        "specialization":"Computer Science and Engineering with Specialization Data Analytics",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"GPA: 9.49"
+      },
+      {
+        "UniversityName":"Air Force School, Pune (CBSE)",
+        "specialization":"12th (Science Stream)",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":"Grade: 89%"
+      },
+      {
+        "UniversityName":"Army Public School, Dhar Road (CBSE)",
+        "specialization":"10th",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2019",
+        "Achievements":"Grade: 86%"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Computer Society Of India(CSI)",
+        "specialization":"Lead of V-Code Department",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2022",
+      },
+      {
+        "CompanyName":"Coding Ninja",
+        "specialization":"Entrepreneur",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2022",
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"JSON"
+      },
+      {
+        "skillname":"XML"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      
+    ],
+
+  }
+  
+  export default resumeData
